fb390c2f46bb798f90889134c22da66cf97c1a67
lldhz <lldhz@126.com>
2016-03-10 13:53:34 +0800

    Merge remote-tracking branch 'origin/master' 极光推送

--- src/main/java/com/haalthy/service/controller/oss/OssController.java
+++ src/main/java/com/haalthy/service/controller/oss/OssController.java
@@ -1,8 +1,17 @@
 package com.haalthy.service.controller.oss;
 
+import com.aliyun.oss.ClientException;
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.model.ObjectMetadata;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import com.aliyuncs.sts.model.v20150401.AssumeRoleRequest;
+import com.aliyuncs.sts.model.v20150401.AssumeRoleResponse;
 import com.haalthy.service.controller.Interface.OSSFile;
+import com.haalthy.service.controller.Interface.Response;
 import com.haalthy.service.oss.OSSFileOperate;
 import com.haalthy.service.oss.OSSSetting;
 import com.haalthy.service.oss.RefreshImgPath;
@@ -21,7 +30,7 @@ import java.util.List;
  * Created by Ken on 2015-12-28.
  */
 @Controller
-@RequestMapping("/open/oss")
+@RequestMapping("/open/oss/upload")
 public class OssController {
 
     //private OssFile file;
@@ -29,6 +38,43 @@ public class OssController {
     private transient RefreshImgPath refreshImgPath;
     private OSSSetting setting ;
     protected Logger logger = Logger.getLogger(this.getClass());
+
+    // 目前只有"cn-hangzhou"这个region可用, 不要使用填写其他region的值
+    public static final String REGION_CN_HANGZHOU = "cn-hangzhou";
+    // 当前 STS API 版本
+    public static final String STS_API_VERSION = "2015-04-01";
+
+    static AssumeRoleResponse assumeRole(String accessKeyId, String accessKeySecret,
+                                         String roleArn, String roleSessionName, String policy,
+                                         ProtocolType protocolType) throws ClientException {
+        try {
+            // 创建一个 Aliyun Acs Client, 用于发起 OpenAPI 请求
+            IClientProfile profile = DefaultProfile.getProfile(REGION_CN_HANGZHOU, accessKeyId, accessKeySecret);
+            DefaultAcsClient client = new DefaultAcsClient(profile);
+
+            // 创建一个 AssumeRoleRequest 并设置请求参数
+            final AssumeRoleRequest request = new AssumeRoleRequest();
+            request.setVersion(STS_API_VERSION);
+            request.setMethod(MethodType.POST);
+            request.setProtocol(protocolType);
+
+            request.setRoleArn(roleArn);
+            request.setRoleSessionName(roleSessionName);
+            request.setPolicy(policy);
+
+            // 发起请求，并得到response
+            AssumeRoleResponse response = new AssumeRoleResponse();
+            try {
+                 response = client.getAcsResponse(request);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        } catch (ClientException e) {
+            throw e;
+        }
+    }
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST, headers = "Accept=application/json",
             produces = {"application/json"})
@@ -130,5 +176,66 @@ public class OssController {
         client.shutdown();
 
         return 0;
+    }
+
+
+    @RequestMapping(value = "/getToken", method = RequestMethod.GET, headers = "Accept=application/json",
+            produces = {"application/json"})
+    @ResponseBody
+    public Response ossSTSGetToken()
+    {
+        Response res = new Response();
+        // 只有 RAM用户（子账号）才能调用 AssumeRole 接口
+        // 阿里云主账号的AccessKeys不能用于发起AssumeRole请求
+        // 请首先在RAM控制台创建一个RAM用户，并为这个用户创建AccessKeys
+        String accessKeyId = "CQblESpYp6rvGUqK";
+        String accessKeySecret = "DdVDpEfJXWOWxvBR9lEkA3aANt1OpK";
+
+        // AssumeRole API 请求参数: RoleArn, RoleSessionName, Polciy, and DurationSeconds
+
+        // RoleArn 需要在 RAM 控制台上获取
+        String roleArn = "acs:ram::1453842368624030:role/aliyunosstokengeneratorrole";
+
+        // RoleSessionName 是临时Token的会话名称，自己指定用于标识你的用户，主要用于审计，或者用于区分Token颁发给谁
+        // 但是注意RoleSessionName的长度和规则，不要有空格，只能有'-' '_' 字母和数字等字符
+        // 具体规则请参考API文档中的格式要求
+        String roleSessionName = "external-username";
+
+        String policy = "{\n" +
+                "    \"Version\": \"1\", \n" +
+                "    \"Statement\": [\n" +
+                "        {\n" +
+                "            \"Action\": [\n" +
+                "                \"oss:*\" \n" +
+                "            ], \n" +
+                "            \"Resource\": [\n" +
+                "                \"acs:*\"\n" +
+                "            ], \n" +
+                "            \"Effect\": \"Allow\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+
+        // 此处必须为 HTTPS
+        ProtocolType protocolType = ProtocolType.HTTPS;
+
+        try {
+            final AssumeRoleResponse response = assumeRole(accessKeyId, accessKeySecret,
+                    roleArn, roleSessionName, policy, protocolType);
+
+            System.out.println("Expiration: " + response.getCredentials().getExpiration());
+            System.out.println("Access Key Id: " + response.getCredentials().getAccessKeyId());
+            System.out.println("Access Key Secret: " + response.getCredentials().getAccessKeySecret());
+            System.out.println("Security Token: " + response.getCredentials().getSecurityToken());
+            res.setResult(1);
+            res.setResultDesp("获取OSS Token成功");
+            res.setContent(response);
+        } catch (ClientException e) {
+            res.setResult(-1);
+            res.setResultDesp("获取OSS Token失败");
+            res.setContent(null);
+            System.out.println("Failed to get a token."+e);
+        }
+        return res;
     }
 }
