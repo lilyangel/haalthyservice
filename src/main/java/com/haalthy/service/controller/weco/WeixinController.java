18e6b1ab1c0acbf2095030837995040c89f52935
lldhz <lldhz@126.com>
2016-03-18 18:21:02 +0800

    通过微信授权CODE获取用户信息

--- src/main/java/com/haalthy/service/controller/weco/WeixinController.java
+++ src/main/java/com/haalthy/service/controller/weco/WeixinController.java
@@ -3,6 +3,7 @@ package com.haalthy.service.controller.weco;
 import com.haalthy.service.controller.Interface.Response;
 import com.haalthy.service.controller.Interface.StringRequest;
 import com.haalthy.service.controller.Interface.WeixinConfig;
+import com.haalthy.service.controller.Interface.WeixinUserInfo;
 import com.haalthy.service.openservice.WeixinService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -53,6 +55,35 @@ public class WeixinController {
         return response;
     }
 
+    @RequestMapping(value = "/getUserInfo", method = RequestMethod.POST, headers = "Accept=application/json",
+            produces = {"application/json"})
+    @ResponseBody
+    public Response getUserInfoOpen(@RequestBody StringRequest request)
+    {
+        Response response = new Response();
+        WeixinUserInfo wx = new WeixinUserInfo();
+        try {
+            Map<String,String> accToken = weixinService.getAccessToken(request.getContent());
+            Map<String,String> userinfo = weixinService.getUserInfo(accToken.get("access_token"),accToken.get("openid"));
+            wx.setOpenid(userinfo.get("openid"));
+            wx.setCity(userinfo.get("city"));
+            wx.setCountry(userinfo.get("country"));
+            wx.setHeadimgurl(userinfo.get("headimgurl"));
+            wx.setNickname(userinfo.get("nickname"));
+            wx.setProvince(userinfo.get("province"));
+            wx.setUnionid(userinfo.get("unionid"));
+            wx.setSex(Integer.getInteger(userinfo.get("sex")));
+            response.setResult(1);
+            response.setResultDesp("获取签名成功");
+            response.setContent(wx);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setResult(-1);
+            response.setResultDesp("获取签名失败");
+            response.setContent(null);
+        }
+        return response;
+    }
     @RequestMapping(value = "/getSignExample", method = RequestMethod.POST, headers = "Accept=application/json",
             produces = {"application/json"})
     @ResponseBody
