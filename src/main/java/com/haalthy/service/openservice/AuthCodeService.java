a46417f64c6d894b3e55c41eefec29f2eadc40bf
Ken <lldhz@126.com>
2016-04-15 00:04:29 +0800

    email authcode

--- src/main/java/com/haalthy/service/openservice/AuthCodeService.java
+++ src/main/java/com/haalthy/service/openservice/AuthCodeService.java
@@ -1,6 +1,7 @@
 package com.haalthy.service.openservice;
 
 import com.haalthy.service.cache.RedisCache;
+import com.haalthy.service.common.ConfigLoader;
 import com.haalthy.service.common.DateUtils;
 import com.haalthy.service.common.EncodeUtil;
 import com.haalthy.service.domain.EMail;
@@ -10,9 +11,20 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+//import java.io.UnsupportedEncodingException;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
+
+
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.JavaMailSenderImpl;
+//import org.springframework.mail.javamail.MimeMessageHelper;
+
 
 /**
  * Created by Ken on 2016-01-05.
@@ -20,7 +32,7 @@ import java.util.List;
 @Service
 public class AuthCodeService {
     protected Logger logger=Logger.getLogger(this.getClass());
-    private  static String eMailTemplate = "<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/></head><body><div>尊敬的用户：</div><div>&nbsp; &nbsp; 您好！</div><div>&nbsp; &nbsp; 您重置密码的验证码为：<b><span style=\"color: rgb(0, 128, 0);\">&#8203;{authCode}</span>（不区分大小写）</b></div><div>&nbsp; &nbsp; 验证码有效期为2天（有效期至：<span style=\"color: rgb(0, 128, 0);\"><b>{validity}</b></span>），请尽快进入APP重置您的密码。</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;haalthy团队</div></body>";
+    private  static String eMailTemplate = "<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/></head><body><div>尊敬的用户：</div><div>&nbsp; &nbsp; 您好！</div><div>&nbsp; &nbsp; 您验证码为：<b><span style=\"color: rgb(0, 128, 0);\">&#8203;{authCode}</span>（不区分大小写）</b></div><div>&nbsp; &nbsp; 验证码有效期为2天（有效期至：<span style=\"color: rgb(0, 128, 0);\"><b>{validity}</b></span>），请尽快验证您的验证码。</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;haalthy团队</div></body>";
 
     @Autowired
     private EMailMapper eMailMapper;
@@ -28,11 +40,115 @@ public class AuthCodeService {
     @Autowired
     private RedisCache redisCache;
 
+    private ConfigLoader configLoader = ConfigLoader.getInstance();
 
+    private String from = null;
+    private String host = null;
+    private String port = "25";
+    private String username=null;
+    private String password = null;
+    private String title = null;
+    private String content = null;
+    private String type = null;
+//    private JavaMailSender javaMailSender;
+
+    private void getMailConfig(String sender,String messager)
+    {
+        host = configLoader.getConfigProperty("email."+sender+".host");
+        from = configLoader.getConfigProperty("email."+sender+".from");
+        port = configLoader.getConfigProperty("email."+sender+".port");
+        username = configLoader.getConfigProperty("email."+sender+".username");
+        password = configLoader.getConfigProperty("email."+sender+".password");
+        title = configLoader.getConfigProperty("email."+messager+".title");
+        content = configLoader.getConfigProperty("email."+messager+".content");
+        type = configLoader.getConfigProperty("email."+messager+".type");
+
+        logger.error(host+"\n"+from+"\n"+port+"\n"+username+"\n"+password+"\n");
+    }
+
+    private String setContent(String content,Map<String,String> params)
+    {
+        for ( Map.Entry<String, String> entry:params.entrySet()
+                ) {
+            content = content.replace("{"+entry.getKey()+"}",entry.getValue());
+        }
+        //String result = content;
+        return content;
+    }
+
+//    public void javaSendMail(String receiver, Map<String,String>params) throws MessagingException,UnsupportedEncodingException
+//    {
+//        getMailConfig("info","authcode");
+//        JavaMailSender javaMailSender = new JavaMailSenderImpl();
+//        MimeMessage msg =javaMailSender.createMimeMessage();
+//
+//        MimeMessageHelper messageHelper = new MimeMessageHelper(msg, true,
+//                "UTF-8");
+//        // messageHelper.setFrom(mailBean.getFrom());
+//        try {
+//            messageHelper.setFrom(from, username);
+//        } catch (UnsupportedEncodingException e) {
+//            logger.error(e);
+//
+//        }
+//        messageHelper.setSubject(title);
+//        messageHelper.setTo(receiver);
+//        messageHelper.setText(setContent(content,params), true); // html: true
+//        javaMailSender.send(msg);
+//    }
+
+    public void sendEmail(String receiver, Map<String,String>params) throws MessagingException,UnsupportedEncodingException
+    {
+        try {
+            getMailConfig("info","authcode");
+            String contentReal = setContent(content, params);
+            Properties props = new Properties();
+            props.put("mail.smtp.host", host);
+            //props.put("mail.smtp.starttls.enable", "true");// 使用 STARTTLS安全连接
+            props.put("mail.smtp.port", port); //google使用465或587端口
+            props.put("mail.smtp.auth", "true"); // 使用验证
+            // props.put("mail.debug", "true");
+
+            // 根据邮件会话属性和密码验证器构造一个发送邮件的session
+            Session sendMailSession = Session.getInstance(props, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username,password);
+                }
+            });
+
+            logger.error(sendMailSession);
+            // 创建邮件对象
+            Message msg = new MimeMessage(sendMailSession);
+
+            // 发件人
+            msg.setFrom(new InternetAddress(from));
+
+            msg.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiver));
+
+            msg.setSubject(MimeUtility.encodeText(title, "UTF-8", "B"));
+            if (type == "txt") {
+                msg.setText(contentReal);
+            } else {
+                msg.setContent(contentReal, "text/html;charset=utf-8");
+            }
+
+            logger.error(msg);
+            Transport.send(msg);
+        }
+        catch (MessagingException e)
+        {
+            logger.error(e);
+        }
+    }
 
     public int addEmailAuthCode(String toEmail) throws Exception {
         int iReturn = 0;
         String strAuthCode = EncodeUtil.getAuthCode(8);
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("authCode",strAuthCode);
+        map.put("validity",DateUtils.addDay2(new Date(), 2));
+        sendEmail(toEmail.toLowerCase(),map);
         String context = (eMailTemplate.replace("{authCode}", strAuthCode)).replace("{validity}", DateUtils.addDay2(new Date(), 2));
         EMail eMail = new EMail();
         eMail.setContent(context);
